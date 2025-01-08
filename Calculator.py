ab585cfca03d327d209206f20118d14678b188c0
Aditya AV <adityaav80@gmail.com>
2025-01-08 22:42:33 +0530

    Revert "Added multiply and divide functionalities both by me"

--- Calculator.py
+++ Calculator.py
@@ -10,12 +10,6 @@ class Calculator:
     def subtract(self, a, b):
         return a - b
 
-    def divide(self, a, b):
-        return a / b
-
-    def mul(self,a,b):
-        return a*b*1000
-
     def mod(self, a,b):
         return a % b
     
@@ -28,7 +22,3 @@ if __name__=='__main__':
 
     print(calculator.add(a,b))
     print(calculator.subtract(a,b))
-
-    print(calculator.divide(a,b))
-    print(calculator.mul(a,b))
-    print("HELLO WORLD")
